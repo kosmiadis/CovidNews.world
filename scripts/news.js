e82a4e3e9f61456b8500e68b3b384c05d6cfc293
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-10-28 11:29:56 +0300

    Added

---
+++ scripts/news.js
@@ -0,0 +1,29 @@
+let covidNews = {
+
+    options: {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '1b7197f7a5msh77b06ed09b7fe0cp156c3bjsn6470e99d7eda',
+            'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
+        }
+    },
+
+    fetchNews: function () {
+        fetch('https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/0', this.options)
+        .then(response => response.json())
+        .then(response => {
+            this.displayNews(response)
+            
+        }) 
+            
+    },
+
+    displayNews: function (news) {
+        console.log(news)
+    }   
+}
+
+window.addEventListener('load', e => {
+    covidNews.fetchNews()
+})
+    
