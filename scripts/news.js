6daa1bc57c8437f46bf9b6ad4b374db4fcd0d2af
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-10-28 13:43:50 +0300

    Add

--- scripts/news.js
+++ scripts/news.js
@@ -1,3 +1,5 @@
+const newsContainer = document.querySelector('.news')
+
 let covidNews = {
 
     options: {
@@ -7,6 +9,8 @@ let covidNews = {
             'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
         }
     },
+
+    
 
     fetchNews: function () {
         fetch('https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/0', this.options)
@@ -20,6 +24,24 @@ let covidNews = {
 
     displayNews: function (news) {
         console.log(news)
+         
+
+        function displayLatestNews (news) {
+            for (const n of news.news) {
+                document.querySelector('.news').innerHTML += 
+                `
+                    <div class='new clickable'>
+                        <p class="publ-date">Published ${n.pubDate.slice(0,10)}</p>
+                        <img src=${n.urlToImage}>
+                        <p class="new-title">${n.title}</p>
+                        <p class="content">${n.content}</p>
+                    </div>
+                `     
+            }
+            
+        }
+
+        displayLatestNews(news)
     }   
 }
 
